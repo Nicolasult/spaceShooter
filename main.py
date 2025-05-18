f70dea6661771d7864a7a1d137a339826e52777a
nicolasult <nicolas.ultrillo@gmail.com>
2025-05-18 17:30:05 +0200

    Add rectangle logic to the game

--- main.py
+++ main.py
@@ -19,20 +19,43 @@ x = 100
 # Import an image
 
 player_surf = pygame.image.load(join("images", "player.png")).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = 1
+
 star_surf = pygame.image.load(join("images", "star.png")).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
+meteor_surf = pygame.image.load(join("images", "meteor.png")).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load(join("images", "laser.png")).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
+
 while running:
+
+    # Event loop
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # Display the game
+
     display_surface.fill("darkgray")
-    display_surface.blit(player_surf, (x, 150))
-    x+= 0.1
 
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
+
+    display_surface.blit(meteor_surf, (meteor_rect))
+    display_surface.blit(laser_surf, (laser_rect))
+
+    # Player movement
+
+    player_rect.x += player_direction * 0.4
+
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surface.blit(player_surf, (player_rect))
 
     pygame.display.update()
 
