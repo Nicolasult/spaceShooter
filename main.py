01f5ecdaeec1e8b120a8a6c9b437cbf9802629e9
nicolasult <nicolas.ultrillo@gmail.com>
2025-05-20 21:03:45 +0200

    Add mask to the player

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ class Player(pygame.sprite.Sprite):
         self.laser_shoot_time = 0
         self.cooldown_duration = 400
 
+        self.mask = pygame.mask.from_surface(self.image)
+        
+
     def laser_timer(self):
         if not self.can_shoot:
             current_time = pygame.time.get_ticks()
@@ -48,6 +51,7 @@ class Laser(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(midbottom = pos)
+
         
     def update(self, dt):
         self.rect.centery -= 400 * dt
@@ -59,11 +63,12 @@ class Meteor(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(center = pos)
+
         self.start_time = pygame.time.get_ticks()
         self.lifetime = 3000
         self.direction = pygame.Vector2(uniform(-0.5, 0.5) , 1)
         self.speed = randint(400, 500)
-    
+
     def update(self, dt):
         self.rect.center += self.direction * self.speed * dt
         if pygame.time.get_ticks() - self.start_time >= self.lifetime:
@@ -72,7 +77,7 @@ class Meteor(pygame.sprite.Sprite):
 def collisions():
     global running
 
-    collision_sprites = pygame.sprite.spritecollide(player, meteor_sprites, True)
+    collision_sprites = pygame.sprite.spritecollide(player, meteor_sprites, True, pygame.sprite.collide_mask)
     if collision_sprites:
         running = False
     
